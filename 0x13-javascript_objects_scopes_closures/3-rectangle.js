36f6ba68413b0b0e2f79665dccab24b552ca92e4
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-09-12 13:48:12 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-const r1 = new Rectangle(2, 3);
-r1.print();
-
-const r2 = new Rectangle(10, 5);
-r2.print();
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(('X'.repeat(this.width)));
+    }
+  }
+};
